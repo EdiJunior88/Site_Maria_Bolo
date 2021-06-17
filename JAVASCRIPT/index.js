a33d95126614eefa4efdd5a1b61a548875e23767
EdiJunior88 <er.moura88@yahoo.com.br>
2021-06-17 17:10:39 -0300

    JAVASCRIPT

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,24 +1,10 @@
 /* Código SlideShow Automático com Efeito Fade */
 
-var i = 0;
-var images = [];
-var time = 3000;
-
-images[0] = 'imagem01.jpg';
-images[1] = 'imagem02.jpg';
-images[2] = 'imagem03.jpg';
-
-function changeImage() {
-  document.hasChildNodes.src = images[i];
-
-  if (i < images.length - 1) {
-    i++;
-  }
-  else {
-    i = 0;
-  }
-
-  setTimeout("changeImage()", time);
-}
-
-window.onload = changeImage;
+$(function(){
+  $('.container_slideshow img:gt(0)').hide();
+  setInterval(function(){
+    $('.container_slideshow :first-child').fadeOut()
+       .next('img').fadeIn()
+       .end().appendTo('.container_slideshow');}, 
+    3000);
+});
